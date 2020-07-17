87e0263384c468d8e01f5af63f070e79699ceed2
robhami <50372518+robhami@users.noreply.github.com>
2020-07-17 12:10:02 +0400

    fix https error

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -4,6 +4,7 @@ import CardList from '../Components/Cardlist';
 import SearchBox from '../Components/SearchBox';
 import Scroll from '../Components/Scroll';
 import './App.css';
+import ErrorBoundary from '../Components/ErrorBoundary';
  
 class App extends Component {
 
@@ -18,7 +19,7 @@ class App extends Component {
     
     componentDidMount() {
 
-      fetch ('http://jsonplaceholder.typicode.com/users')
+      fetch ('https://jsonplaceholder.typicode.com/users')
         .then (response => {
           return response.json();
         })
@@ -56,7 +57,9 @@ class App extends Component {
            <h1 className="f1">Oilees</h1>
             <SearchBox searchChange={this.onSearchChange}/>
             <Scroll>
-           <CardList users={filteredUsers}/>
+              <ErrorBoundary>
+                <CardList users={filteredUsers}/>
+              </ErrorBoundary>
            </Scroll>
           </div>
         );
