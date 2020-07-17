7499594f52331e301f98715c2b417d0c215cfc8c
robhami <50372518+robhami@users.noreply.github.com>
2020-07-17 12:05:59 +0400

    Updates

---
+++ precache-manifest.bad86a216079f6527fe99e7b01ed8544.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c1f12353e9e6c70a82ecd4f9085c177",
+    "url": "/oilees_react_website/index.html"
+  },
+  {
+    "revision": "808bcac86816cbdc03df",
+    "url": "/oilees_react_website/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "961ac2bebbb01aba7058",
+    "url": "/oilees_react_website/static/css/main.07780428.chunk.css"
+  },
+  {
+    "revision": "808bcac86816cbdc03df",
+    "url": "/oilees_react_website/static/js/2.7d54c52c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/oilees_react_website/static/js/2.7d54c52c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "961ac2bebbb01aba7058",
+    "url": "/oilees_react_website/static/js/main.295d7a94.chunk.js"
+  },
+  {
+    "revision": "8592d9d6bdaf7157366c",
+    "url": "/oilees_react_website/static/js/runtime-main.fca3b31e.js"
+  },
+  {
+    "revision": "7bf226306214181d5e917ed5bda2301c",
+    "url": "/oilees_react_website/static/media/norwester.7bf22630.otf"
+  }
+]);
